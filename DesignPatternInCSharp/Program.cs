c3e1d124bafd0458dff40b3a6a01536d124461ed
Pham Duc Minh <phamducminh1990@gmail.com>
2016-12-21 11:21:36 +0700

    add

---
+++ DesignPatternInCSharp/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace DesignPatternInCSharp.Generic
+{
+    internal class GenericMethod
+    {
+        private static int ConvertToInt(object value, object defaultValue)
+        {
+            if (value == null || value == DBNull.Value)
+            {
+                return Convert.ToInt32(defaultValue);
+            }
+            else
+            {
+                return Convert.ToInt32(value);
+            }
+        }
+
+        private static DateTime ConvertToDateTime(object value, object defaultValue)
+        {
+            if (value == null || value.Equals(DBNull.Value))
+            {
+                return Convert.ToDateTime(defaultValue);
+            }
+            else
+            {
+                return Convert.ToDateTime(value);
+            }
+        }
+
+        // Combine to one generic method
+        private static T ConvertTo<T>(object value, object defaultValue)
+        {
+            if (value == null || value.Equals(DBNull.Value))
+            {
+                return (T)Convert.ChangeType(defaultValue, typeof(T));
+            }
+            else
+            {
+                return (T)Convert.ChangeType(value, typeof(T));
+            }
+        }
+
+        /// <summary>
+        /// Action
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ConvertToInt("123", default(int))+1);
+            Console.WriteLine(ConvertToDateTime("12/30/2015", default(DateTime)));
+            Console.WriteLine(ConvertTo<int>("999", default(int))+1);
+            Console.WriteLine(ConvertTo<DateTime>("11/19/1111", default(DateTime)));
+
+            Console.Read();
+        }
+    }
+}
---
+++ hashing/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace hashing
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // HashPassword with no salt auto generate ramdom salt
+            // So each time we HashPassword the hash result is differen !
+
+            string strSalt = BCryptUtil.GenerateSalt();
+            string strSalt2 = BCryptUtil.GenerateSalt(10);
+            string strSaltRandom = "$2a$10$KL4bRGKuNMC5kNFLj8bjbu";
+            string password1 = BCryptUtil.HashPassword("Hello12#", strSaltRandom);
+
+            //bool isValid = BCrypt.Net.BCrypt.Verify("Hello12#", BCryptUtil.HashPassword("Hello12#"));
+            //Console.WriteLine(strSalt);
+            //Console.WriteLine(strSalt2);
+            Console.WriteLine(password1);
+            //Console.WriteLine(isValid);
+
+            Console.Read();
+
+            //satl : $2a$10$KL4bRGKuNMC5kNFLj8bjbu
+
+            //hash by salt 1 : $2a$10$KL4bRGKuNMC5kNFLj8bjbu00p4t9szBOhrOy1nbyR6D/Be93UK69y
+
+            //hash by salt 2 : $2a$10$KL4bRGKuNMC5kNFLj8bjbu00p4t9szBOhrOy1nbyR6D/Be93UK69y
+        }
+    }
+
+    public class BCryptUtil
+    {
+        public static string GenerateSalt()
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt();
+        }
+
+        public static string GenerateSalt(int workFactor)
+        {
+            return BCrypt.Net.BCrypt.GenerateSalt(workFactor);
+        }
+
+        public static string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static string HashPassword(string password, string salt)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password, salt);
+        }
+    }
+}
