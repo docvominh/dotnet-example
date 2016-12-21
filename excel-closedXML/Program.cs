a86d4360c4d3938d459c9997de560a4f183912b0
Pham Duc Minh <phamducminh1990@gmail.com>
2016-12-21 11:02:17 +0700

    add

---
+++ excel-closedXML/Program.cs
@@ -0,0 +1,46 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excel_closedXML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create excel file in memory
+            XLWorkbook file = new XLWorkbook();
+            // Add Sheet
+            file.Worksheets.Add("First Sheet");
+            // Get Sheet
+            IXLWorksheet sheet = file.Worksheet("First Sheet");
+            // Add cell value
+            sheet.Cell("A1").Value = "I am A1";
+            sheet.Cell("A3").Value = "I am A3";
+            // Add some style
+            sheet.ColumnWidth = 30;
+            sheet.Row(1).Style.Fill.BackgroundColor = XLColor.Green;
+
+            // Save file to hard disk
+            file.SaveAs(@"D:\closeXML-example.xlsx");
+
+            // Release resource
+            sheet.Dispose();
+            file.Dispose();
+
+            // Open excel file
+            XLWorkbook fileOpen = new XLWorkbook(@"D:\closeXML-example.xlsx");
+            IXLWorksheet openSheet = file.Worksheet("First Sheet");
+
+            Console.WriteLine(string.Format("Total used row (Contain blank row) = {0}", openSheet.LastRowUsed().RowNumber()));
+            Console.WriteLine(string.Format("Total used row  = {0}", openSheet.RowsUsed().Count()));
+            Console.WriteLine(string.Format("Value of Cell A1  = {0}", openSheet.Cell("A1").Value));
+
+
+            Console.Read();
+        }
+    }
+}
---
+++ http-request/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace http_request
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Html return
+            string url = "https://www.google.com";
+
+            // Json return
+            string url2 = "http://www.w3schools.com/angular/customers.php";
+
+            string result = string.Empty;
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create((url2));
+            request.Method = "GET";
+            request.ContentLength = 0;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream stream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(stream);
+                    result = reader.ReadToEnd();
+                }
+            }
+
+            // Write out all html content
+            Console.WriteLine(result);
+            Console.Read();
+        }
+    }
+}
