1f074b1b8234ed0b947d1d2b08f5339e9e6b932e
Pham Duc Minh <phamducminh1990@gmail.com>
2016-12-23 17:02:24 +0700

    update

---
+++ NUnit/SomeBusiness.cs
@@ -0,0 +1,15 @@
+﻿namespace NUnit
+{
+    public class SomeBusiness
+    {
+        public int GetSomeNumber()
+        {
+            return 99;
+        }
+
+        public string GetTheMostHandsomeMan()
+        {
+            return "Minh";
+        }
+    }
+}
---
+++ NUnit/SomeBusinessTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+
+namespace NUnit
+{
+
+    /* 
+     * 1. Install Nunit from Nuget Package
+     * 2. Go to "Tools -> Extensions and Updates -> Online" and search for "NUnit3 Test Adapter" and then install to show testcase in Test Explorer
+     */
+    [TestFixture]
+    public class SomeBusinessTest
+    {
+        private SomeBusiness sb = new SomeBusiness();
+
+        [Test]
+        public void GetSomeNumberTest()
+        {
+            Assert.That(sb.GetSomeNumber(), Is.EqualTo(99));
+        }
+
+        [Test]
+        public void GetTheMostHandsomeManTest()
+        {
+            Assert.That(sb.GetTheMostHandsomeMan(), Is.EqualTo("Minh"));
+        }
+    }
+}
